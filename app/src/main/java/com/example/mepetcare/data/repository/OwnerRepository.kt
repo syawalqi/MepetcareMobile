939691b2838f272bfde74dced6b5c81eef94d2db
syawalqi <galihmaulanasyawalqi@gmail.com>
2026-01-07 01:47:24 +0700

    minor fixes

--- app/src/main/java/com/example/mepetcare/data/repository/OwnerRepository.kt
+++ app/src/main/java/com/example/mepetcare/data/repository/OwnerRepository.kt
@@ -5,5 +5,5 @@ import com.example.mepetcare.data.remote.RetrofitClient
 class OwnerRepository {
     suspend fun getOwners() = RetrofitClient.ownerApi.getOwners()
 
-    suspend fun getOwnerPets(ownerId: Int) = RetrofitClient.patientApi.getOwnerPets(ownerId)
+    suspend fun getOwnerPets(ownerId: Int) = RetrofitClient.ownerApi.getOwnerPets(ownerId)
 }
