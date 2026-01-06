d5a7537a823f7419a074899b03baece6db270341
syawalqi <galihmaulanasyawalqi@gmail.com>
2026-01-07 01:46:33 +0700

    membuat AdminHomeScreen.kt

--- app/src/main/java/com/example/mepetcare/view/admin/AdminHomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mepetcare.view.admin
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun AdminHomeScreen() {
-    Text("Admin Home")
-}
