67a2343cbd19b5542cc817ecc400a1ce47a6c82a
syawalqi <galihmaulanasyawalqi@gmail.com>
2026-01-07 01:46:53 +0700

    minor fixes

--- app/src/main/java/com/example/mepetcare/data/remote/DoctorApi.kt
+++ app/src/main/java/com/example/mepetcare/data/remote/DoctorApi.kt
@@ -1,20 +1,32 @@
 package com.example.mepetcare.data.remote
 
-import okhttp3.Response
+import com.example.mepetcare.data.model.MedicalRecord
+import com.example.mepetcare.data.model.Owner
+import com.example.mepetcare.data.model.Patient
+import com.example.mepetcare.data.model.ServiceType
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface DoctorApi {
-    // To list owners and their pets
     @GET("userp/list")
     suspend fun getOwners(): Response<List<Owner>>
 
     @GET("userp/{id}/pasien")
     suspend fun getOwnerPets(@Path("id") id: Int): Response<List<Patient>>
 
-    // FR-31: Save Medical Record (Pemeriksaan)
-    @POST("pemeriksaan/create")
-    suspend fun createExamination(@Body data: Map<String, String>): Response<Unit>
+    // NEW: Get historical records
+    @GET("medical-records/pasien/{idpasienk}")
+    suspend fun getMedicalHistory(@Path("idpasienk") id: Int): Response<List<MedicalRecord>>
+
+    // NEW: Create record (Matches your controller)
+    @POST("medical-records/create")
+    suspend fun createMedicalRecord(@Body data: Map<String, String>): Response<Unit>
+
+    // Inside DoctorApi interface
+    @GET("services/list") // Path based on your router export
+    suspend fun listServices(): retrofit2.Response<List<ServiceType>>
+
 }
